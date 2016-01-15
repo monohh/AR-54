2290c15b154fe95ffe565cc70e37ba4e84d45fdd
Shu Sam Chen <research.hyperspace@gmail.com>
2016-01-15 21:42:35 +0000

    [DevPro] Added trivial class to verify test functionality

---
+++ AR-54.Test.Core/EchoChamberTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AR_54.Common.Test;
+
+namespace AR_54.Test.Core
+{
+    [TestClass]
+    public class ShoutTests
+    {
+        [TestMethod]
+        public void Test_ShoutLoudly_WithLowerCase()
+        {
+            // Arrange
+            string input = "fourwordsalluppercase";
+
+            // Act
+            string output = EchoChamber.ShoutLoudly(input);
+
+            // Assert
+            Assert.AreEqual(input.ToUpper(), output);
+        }
+
+        [TestMethod]
+        public void Test_ShoutLoudly_WithUpperCase()
+        {
+            // Arrange
+            string input = "ONE WORD ALL LOWERCASE";
+
+            // Act
+            string output = EchoChamber.ShoutLoudly(input);
+
+            // Assert
+            Assert.AreEqual(input.ToUpper(), output);
+        }
+    }
+}
---
+++ Assets/Common/Test/EchoChamber.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AR_54.Common.Test
+{
+    public class EchoChamber
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Trivial method to verify correct test suite functionality.
+        /// Returns the input string in all uppercase.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ShoutLoudly(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}

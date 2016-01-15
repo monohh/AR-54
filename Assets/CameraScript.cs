86c5ca3c01d369d8eefb442e118ef2a962eaf6af
Jonathan Stephens-Jones <Jonathan Stephens-Jones>
2016-01-15 20:51:46 +0000

    Fresh Project

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;  //The camera tagged as "MainCamera"
+        if (cam == null)
+        {
+            Debug.LogError("No camera attached to script.");
+        }
+    }
+	
+    void Awake()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (cam != null)
+        {
+            float xAxisValue = Input.GetAxis("Horizontal");
+            float zAxisValue = Input.GetAxis("Vertical");
+
+            /*Remove all but the Y axis rotation from the camera so it translates
+              along the world's x and z axes rather than its own
+            */
+            Quaternion CurrentRotation = cam.transform.rotation;
+            Vector3 ea = CurrentRotation.eulerAngles;
+            cam.transform.rotation = Quaternion.Euler(0, ea.y, 0);
+
+            //Perform the translation, then restore the original rotation
+            cam.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+            cam.transform.rotation = CurrentRotation;
+
+        }
+
+        
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+AR-54
+=================
+[![Build Status](https://hsar.visualstudio.com/DefaultCollection/_apis/public/build/definitions/078e86c7-97a7-4a2e-a516-e6fdec3f9c6c/1/badge)](https://hsar.visualstudio.com/DefaultCollection/AR54/_build#_a=completed&definitionType=2&definitionId=1)
+
+Main repository for the AR-54 game development project.
